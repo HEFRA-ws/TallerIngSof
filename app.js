cb5c5d203a44bedb4bd7caa597b84cb3b310d1d3
HEFRA-ws <hefra.nt@gmail.com>
2026-08-20 19:30:26 -0500

    feat(mejoras): conecta la funcion de resaltado al cargar la pagina

--- app.js
+++ app.js
@@ -7,3 +7,4 @@ document.addEventListener("DOMContentLoaded", saludar);
 function resaltarMenuActivo() {
   console.log("Resaltando el enlace activo del menu");
 }
+document.addEventListener("DOMContentLoaded", resaltarMenuActivo);
